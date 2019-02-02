8c49c788a3e72067990f6523523a48710790333f
Alexander Shvets <alexander.shvets@gmail.com>
2019-02-02 11:36:32 -0500

    expose interceptors

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
 import { Rxios, RxiosConfig } from './rxios';
+import { AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse } from "axios";
 export { Rxios, RxiosConfig };
+export { AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse };
